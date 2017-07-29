72fc6c845fabe9a0bd2e4bd20abb7fdc97c0bcb3
Chocolate Unicorn <orkalittlecutewolf@live.com>
2017-07-29 20:33:45 +0200

    Created page structure & some basic datas
    HTML basic structure has been done, some datas have been put in script.js (some real datas, some test ones). This is the beginning.

---
+++ script.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: 'This is a first importance title like - Iolite are not well known',
+    message: 'This is a small text about the use of this web app like - iolites have a beautiful color',
+    subtitle: 'This is a secondary title like - iolite mines',
+    p_subtitle: 'This is a ternary title like - iolite identification',
+    a_subtitle: 'This is another ternary title like - iolite chemical composition',
+    pronouns: [
+      'iel',
+      'ael',
+      'elle',
+      'ellui',
+      'ielle',
+      'ø',
+      'ol',
+      'ul',
+      'il',
+      'they',
+      'ze',
+      'he',
+      'she',
+      'xe',
+      'ae',
+      'aelle',
+      'al'
+    ],
+    accords: [
+      "acc",
+      "acc",
+      "acc",
+      "acc",
+      "acc",
+      "acc",
+      "acc",
+      "acc"
+    ]
+  }
+})
